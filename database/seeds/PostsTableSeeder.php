649553c81be51857cc94a310cdfaefabc3b7016d
simona <palumbosimona118@gmail.com>
2020-03-30 16:59:27 +0200

    seeder

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use App\User;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+    }
+}
