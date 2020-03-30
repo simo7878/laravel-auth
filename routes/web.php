46ba93a8c79f5540d2b2ef6f85c504033566e66c
simona <palumbosimona118@gmail.com>
2020-03-30 19:03:59 +0200

    up

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::Get("/postGuest","PostController@index")->name("posts");
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::name("admin.")->prefix("admin")->namespace("Admin")->middleware('auth')->group(function(){
+  //  Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource("posts","PostController");
+});
